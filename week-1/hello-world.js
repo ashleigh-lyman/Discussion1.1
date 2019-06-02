5e41510128096e148015eb1b60e0195208f5062f
Ashleigh <ajlyman@my365.bellevue.edu>
2019-06-02 13:10:13 -0500

    Added comments and code attribution to hello-world. Created new file named variables-values-output.js

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,19 @@
+/*
+============================
+;Title: Assignment 1.4
+;Author: Ashleigh Lyman
+;Date: June 2, 2019
+;Modified by: 
+;Description: declaring and assigning variables
+;============================
+*/
+
+//Start Program
+
+/*Expected outcome:
+Hello World, my name is Ashleigh Lyman!
+*/
+
+//Output
+
 console.log("Hello World, my name is Ashleigh Lyman!");
