3ab28149d5f646f885715bb687062d7815109db9
Ashleigh <ajlyman@my365.bellevue.edu>
2019-06-02 13:12:52 -0500

    Changed file to Lyman-variables-values-output.js

---
+++ week-1/Lyman-variables-values-output.js
@@ -0,0 +1,35 @@
+/*
+====================
+;Title: Assignment1.4
+;Author: Ashleigh Lyman
+;Date: June 2, 2019
+;Modified by: 
+;Description: Primitive Types
+;====================
+*/
+
+//Start Program
+
+/*
+Expected Outcome: 
+Ashleigh Lyman
+26 years old
+1234 Main Street
+Lincoln
+11324
+*/
+
+//Declare Variables
+
+var firstName = "Ashleigh ";
+var lastName = "Lyman";
+var age = "26 years old";
+var street = "1234 Main Street";
+var city = "Lincoln";
+var zipCode = "11324";
+
+//Output
+
+console.log(firstName + lastName + '\n' + age + '\n' + street + '\n' + city + '\n' + zipCode);
+
+//End of Program
