244984761313771168f5928d4b685afca513a167
Ashleigh <ajlyman@my365.bellevue.edu>
2019-06-12 21:32:45 -0500

    Created new file Lyman-assignment3.1 and added header.js

---
+++ week-3/Lyman-assignment3.1.js
@@ -0,0 +1,42 @@
+/*
+====================
+Title: Exercise 3.1
+Author: Ashleigh Lyman
+Date: 12 June 2019
+Modified by:
+Description: Program using if statement with 2 errors
+;====================
+*/
+
+// Importing and outputting header file required for all JS assignments
+
+var header = require('./Lyman-header.js');
+console.log(header.display('Ashleigh', 'Lyman', 'Exercise 3.1 - Control Statements'));
+
+/*
+Expected outcome:
+
+Ashleigh Lyman
+Exercise 3.1 - Control Statements
+6/12/2019
+
+You have 3 items in your shopping cart
+
+*/
+
+//Start Program
+
+//Declare variable
+var a = 3;
+
+//Begin if statement, if variable does equal 1 then display message.
+if (a ==1);
+
+  console.log('\n' + "You have 1 item in your shopping cart.")
+
+//If else then display variable amount in message.
+else
+  console.log('\n' + "You have " + b + " items in your shopping cart.");
+
+
+//End Program
