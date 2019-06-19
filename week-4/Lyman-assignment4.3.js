d86bdf3ca2c7a2570447e1ac9147b382944e4867
Ashleigh <ajlyman@my365.bellevue.edu>
2019-06-18 22:07:22 -0500

    Modified assignment4.3

--- week-4/Lyman-assignment4.3.js
+++ week-4/Lyman-assignment4.3.js
@@ -1,6 +1,6 @@
 /*
 ====================
-Title: Assignment 4.2 Filtering
+Title: Assignment 4.3 Filtering
 Author: Ashleigh Lyman
 Date: 18 June 2019
 Modified by:
@@ -11,7 +11,7 @@ Description: Filtering assignment
 // Importing and outputting header file required for all JS assignments
 
 var header = require('./Lyman-header.js');
-console.log(header.display('Ashleigh', 'Lyman', 'Exercise 4.2 - Arrays'));
+console.log(header.display('Ashleigh', 'Lyman', 'Exercise 4.3 - Filtering'));
 
 /*
 Expected outcome:
@@ -19,3 +19,47 @@ Ashleigh Lyman
 Exercise 4.3 - Filtering
 Current Date
 */
+
+//Start Program
+
+//Declaring Vehicles array
+var vehicles = ["Car", "Truck", "Van", "Motorcycle", "Bus"];
+
+
+//Function
+function getValue(array, value) {
+  for(var a = 0; a < array.length; a++) {
+    if(array[a] === value) {
+      console.log(array[a]);
+
+    }
+
+  }
+}
+
+//Empty line
+console.log("");
+
+//Output array list
+console.log("--DISPLAYING ARRAY ITEMS--")
+for(var b = 0; b < vehicles.length; b++) {
+  console.log(vehicles[b]);
+}
+
+//Empty line
+console.log("");
+
+
+//Output selected array item
+console.log("--SELECTED ITEM");
+getValue(vehicles, "Motorcycle");
+
+
+//Empty line
+console.log("");
+
+//Output selected array item
+console.log("--SELECTED ITEM");
+getValue(vehicles, "Bus");
+
+//End Program
