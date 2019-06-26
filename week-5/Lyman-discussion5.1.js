2e56b28780e3079b085f9d23e33331bcb0ada9c3
Ashleigh <ajlyman@my365.bellevue.edu>
2019-06-26 18:41:30 -0500

    Created discussion5.1 file for keyed collection program

---
+++ week-5/Lyman-discussion5.1.js
@@ -0,0 +1,35 @@
+/*
+====================
+Title: Discussion 5.1
+Author: Ashleigh Lyman
+Date: 26 June 2019
+Modified by:
+Description: Program using keyed collection
+;====================
+*/
+
+// Importing and outputting header file required for all JS assignments
+
+var header = require('./Lyman-header.js');
+console.log(header.display('Ashleigh', 'Lyman', 'Discussion 5.1 - Keyed Collection program'));
+
+/*
+Expected outcome:
+Ashleigh Lyman
+Exercise 5.1 - Keyed Collection program
+Current Date
+*/
+
+//Start Program
+
+//Array of fruits
+var fruits = new Array('orange', 'apple', 'apple', mango, 'orange', 'mango');
+
+//Blank line
+console.log("");
+
+//Use indexOf to locate and display first mention of selected item
+console.log(fruit.indexOf('mango'));
+
+
+//End Program
